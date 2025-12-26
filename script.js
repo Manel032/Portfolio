cc4b7cd700265cc58f9a9b49fbad0a515a660005
Manel Bouchhioua <manel.bouchhioua@esprit.tn>
2025-12-26 12:17:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Portfolio chargé avec succès");
+
+const btn = document.getElementById("darkMode");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
